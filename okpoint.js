7df35976456d114928e40863fe71dd78002098aa
gongzhanli <35677069+gongzhanli@users.noreply.github.com>
2020-01-17 11:06:24 +0800

    add js point for test

---
+++ okpoint.js
@@ -0,0 +1,155 @@
+var datas = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "properties": {
+                "time": 1,
+                "id": "route1",
+                "name": "路名1"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    120.79948，
+                    31.37601
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "time": 2,
+                "id": "route1",
+                "name": "路名2"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    120.79943，
+                    31.37603
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "time": 3,
+                "id": "route1",
+                "name": "路名3"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    120.79936，
+                    31.37603
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "time": 4,
+                "id": "route1",
+                "name": "路名4"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    120.7993，
+                    31.37604
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "time": 5,
+                "id": "route1",
+                "name": "路名5"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    120.79922，
+                    31.37605
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "time": 6,
+                "id": "route1",
+                "name": "路名6"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    120.79909，
+                    31.37609
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "time": 7,
+                "id": "route1",
+                "name": "路名7"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    120.7989，
+                    31.37613
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "time": 8,
+                "id": "route1",
+                "name": "路名8"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    120.7988，
+                    31.37615
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "time": 9,
+                "id": "route1",
+                "name": "路名9"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    120.7986，
+                    31.37614
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "time": 10,
+                "id": "route1",
+                "name": "路名10"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    120.7984，
+                    31.3761
+                ]
+            }
+        }
+    ]
+};
